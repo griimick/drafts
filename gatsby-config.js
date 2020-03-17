4d26d8a861383d517508e9d62088ba8f13b0a4e1
Soumik Pradhan <soumikpradhan14@gmail.com>
2020-03-18 05:10:01 +0530

    Config prefix path

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,25 @@
 module.exports = {
-  plugins: [
-    {
-      resolve: `gatsby-theme-blog`,
-      options: {},
-    },
-  ],
-  // Customize your site metadata:
-  siteMetadata: {
-    title: `My Drafts`,
-    author: `griimick`,
-    description: `About things I want to understand`,
-    social: [
-      {
-        name: `twitter`,
-        url: `https://twitter.com/griimick`,
-      },
-      {
-        name: `github`,
-        url: `https://github.com/griimick`,
-      },
-    ],
-  },
+	pathPrefix: "/drafts",
+	plugins: [
+		{
+			resolve: `gatsby-theme-blog`,
+			options: {},
+		},
+	],
+	// Customize your site metadata:
+	siteMetadata: {
+		title: `My Drafts`,
+		author: `griimick`,
+		description: `About things I want to understand`,
+		social: [
+			{
+				name: `twitter`,
+				url: `https://twitter.com/griimick`,
+			},
+			{
+				name: `github`,
+				url: `https://github.com/griimick`,
+			},
+		],
+	},
 }
